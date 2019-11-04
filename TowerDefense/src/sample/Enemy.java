644d27602ca770bd3588fa7e13d502807ed509fa
Hajuha <hajuha168@gmail.com>
2019-11-05 00:55:04 +0700

    Add some Enemy.java properties && rename MachineGunTower.java

--- TowerDefense/src/sample/Enemy.java
+++ TowerDefense/src/sample/Enemy.java
@@ -2,6 +2,11 @@ package sample;
 //mở rộng từ đối tượng trò chơi, quản lý các đối tượng quân địch
 public interface Enemy extends GameEntity{
     @Override
+    double health;
+    double speed = 0;
+    double armor = 0;
+    int reward = 0;
+
     void loadImage(String path);
     void move();
     boolean hasDied();
