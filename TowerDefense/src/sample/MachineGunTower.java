fe3be0f6d2e5d171fd7262ed85c77d9af4feb538
Hajuha <hajuha168@gmail.com>
2019-11-05 00:57:52 +0700

    Add some Enemy.java properties && rename MachineGunTower.java

---
+++ TowerDefense/src/sample/MachineGunTower.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class MachineGunTower implements Tower {
+    @Override
+    public void putTower(int x, int y) {
+
+    }
+
+    @Override
+    public void shoot() {
+
+    }
+
+    @Override
+    public boolean findEnemy() {
+        return false;
+    }
+
+    @Override
+    public void loadImage(String path) {
+
+    }
+}
